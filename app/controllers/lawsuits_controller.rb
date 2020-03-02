2315d80fd156203d1abb559ae12862711ce0dda3
Günter <gunterstrikedev@gmail.com>
2020-03-03 00:22:17 +0100

    editing controller

--- app/controllers/lawsuits_controller.rb
+++ app/controllers/lawsuits_controller.rb
@@ -1,8 +1,20 @@
 class LawsuitsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+  @lawsuit = Lawsuit.new
   end
 
   def create
+    Lawsuit.create!(lawsuit_params)
+    redirect_to lawsuit_path
   end
+
+  private
+
+  def lawsuit_params
+    params.require(:lawsuit).permit(:first_name, :last_name, :zip_code , :number_dni , :violence_type, :violence_date, :author_violence, :lawsuit_type, :declaration)
+  end
+
 
 end
