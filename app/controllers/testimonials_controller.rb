bd4f6efac9246f742197269ef6a1bb8a6fd84d5f
Günter <gunterstrikedev@gmail.com>
2020-03-03 19:08:58 +0100

    video etiting, thumbnail gem

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -2,5 +2,6 @@ class TestimonialsController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     @testimonial = Testimony.all
+    @fake_test = VideoThumb::get("http://www.youtube.com/watch?v=iEPTlhBmwRg", "medium")
   end
 end
