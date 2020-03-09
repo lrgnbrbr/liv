3149c2322f65b2cdafc57486dc5577053f316710
Laura Lauret Milazzo <laura.lauret.milazzo@gmail.com>
2020-03-09 19:06:20 +0100

    fix controller

--- app/controllers/pdf_controller.rb
+++ app/controllers/pdf_controller.rb
@@ -17,9 +17,8 @@ class PdfController < ActionMailer::Base
   def lawsuit_send
      @pdf = mail(to: 'liv.asso.364@gmail.com', subject: 'Votre fichier a été envoyé', pdf: 'lawsuit#{id}.pdf')
      @lawsuit_mailer = lawsuit_send('lawsuit#{id}.pdf')
-
-    link_to 'Envoyer le PDF', lawsuit_path(@lawsuit, :format => :pdf, :formtarget => '_blank')
-    end
+    # link_to 'Envoyer le PDF', lawsuit_path(@lawsuit, :format => :pdf, :formtarget => '_blank')
+    # end
   end
 end
 
