2b2715d4833042145041c88a23f86fdf45ef230c
Pat C <patiere@tutanota.com>
2020-03-09 18:35:22 +0100

    suite envoi pdf

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :lawsuits, only: [ :new, :create, :show]
+  resources :lawsuits, only: [ :new, :create, :show, :lawsuit_send]
   resources :organizations
   resources :medical
-
-
-
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
